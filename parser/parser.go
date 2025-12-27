a44482a06042508cdb5c9b43aecf1414b6b6e35f
sendo <aldo.sierrav@gmail.com>
2025-12-27 14:24:37 -0600

    Add support for parse errors and return statements

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 
 import (
+	"fmt"
 	"github.com/aldosierra/monkey/ast"
 	"github.com/aldosierra/monkey/lexer"
 	"github.com/aldosierra/monkey/token"
@@ -10,14 +11,17 @@ import (
 
 type Parser struct {
 	l *lexer.Lexer
-
+	errors []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
@@ -51,6 +55,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -58,7 +64,7 @@ func (p *Parser) parseStatement() ast.Statement {
 
 
 func (p *Parser) parseLetStatement() ast.Statement {
-	stmt := &ast.LetStatement{Token: p.curToken}
+	stmt := &ast.LetStatement{ Token: p.curToken }
 
 	if !p.expectPeek(token.IDENT) {
 		return nil
@@ -69,6 +75,19 @@ func (p *Parser) parseLetStatement() ast.Statement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+
+func (p *Parser) parseReturnStatement() ast.Statement {
+	stmt := &ast.ReturnStatement{ Token: p.curToken }
+
+	p.nextToken()
 
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
@@ -93,5 +112,18 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	}
+	p.peekError(t)
 	return false
+}
+
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
